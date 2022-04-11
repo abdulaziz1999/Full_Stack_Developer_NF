eac4b239a28ec30249eac396fd8d92cc7e81de9d
abdulaziz1999 <azizmentor96@gmail.com>
2022-04-11 08:50:29 +0700

    landing index

--- PHP/landing/detail/home.php
+++ PHP/landing/detail/home.php
@@ -1,5 +1,5 @@
 <main class="main-content  mt-0">
-    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
+    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg mb-7" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
       <span class="mask bg-gradient-dark opacity-6"></span>
       <div class="container">
         <div class="row justify-content-center">
