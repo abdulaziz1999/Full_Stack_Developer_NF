0b4d183882ab6d92bfe15b4ff0212381a36e2be1
abdulaziz1999 <azizmentor96@gmail.com>
2022-04-13 14:12:08 +0700

    page social media

--- PHP/landing/detail/social_media.php
+++ PHP/landing/detail/social_media.php
@@ -3,9 +3,19 @@
     <p>"Be a Beneficial Person for Others".</p>
 </div>
 <div class="social-links text-center mt-4 ">
-  <a href="https://twitter.com/AbdulAz65072470" class="btn btn-primary"><i class="bx bxl-twitter"></i></a>
-  <a href="https://www.facebook.com/aziz.revolusion" class="btn btn-info"><i class="bx bxl-facebook"></i></a>
-  <a href="https://instagram.com/azizakbarpermana" class="btn btn-danger"><i class="bx bxl-instagram"></i></a>
-  <a href="https://aziz212.medium.com" class="btn btn-secondary"><i class="bx bxl-medium"></i></a>
-  <a href="https://www.linkedin.com/in/abdul-aziz-9a64711a7" class="btn btn-primary"><i class="bx bxl-linkedin"></i></a>
+  <a href="https://twitter.com/AbdulAz65072470" target="_blank" type="button" class="btn btn-twitter btn-icon-only rounded-circle">
+      <span class="btn-inner--icon"><i class="fab fa-twitter"></i></span>
+  </a>
+  <a href="https://www.facebook.com/aziz.revolusion" target="_blank" type="button" class="btn btn-facebook btn-icon-only rounded-circle">
+      <span class="btn-inner--icon"><i class="fab fa-facebook"></i></span>
+  </a>
+  <a href="https://instagram.com/azizakbarpermana" target="_blank" type="button" class="btn btn-youtube btn-icon-only rounded-circle">
+    <span class="btn-inner--icon"><i class="fab fa-instagram"></i></span>
+  </a>
+  <a href="https://aziz212.medium.com" target="_blank" type="button" class="btn btn-github btn-icon-only rounded-circle">
+    <span class="btn-inner--icon"><i class="fab fa-medium"></i></span>
+  </a>
+  <a href="https://www.linkedin.com/in/abdul-aziz-9a64711a7" target="_blank" type="button" class="btn btn-linkedin btn-icon-only rounded-circle">
+    <span class="btn-inner--icon"><i class="fab fa-linkedin"></i></span>
+  </a>
 </div>
