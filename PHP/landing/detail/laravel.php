b0a6cbbd7aeb2167c892f750b34189729df075e4
abdulaziz1999 <azizmentor96@gmail.com>
2022-04-13 14:07:30 +0700

    laravel

---
+++ PHP/landing/detail/laravel.php
@@ -0,0 +1,203 @@
+<?php include_once 'detail.php'; ?>
+    <main class="main-content  mt-0">
+        <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
+        <span class="mask bg-gradient-dark opacity-6"></span>
+          <div class="container">
+              <div class="row justify-content-center mb-5">
+                <div class="col-lg-5 text-center mx-auto">
+                    <h1 class="text-white mb-2 mt-5">Welcome!</h1>
+                    <p class="text-lead text-white">Course <b>Full Stack Web Developer</b>.</p>
+                    <a href="javascript:;">
+                      <img src="<?= $img?>" width="100" height="200" class="rounded-circle img-fluid border border-2 border-white">
+                    </a>
+                    <h5 class="text-white mb-2 mt-2"><?= $judul?></h5>
+                </div>
+              </div>
+              <a href="index.php" class="text-white"><i class="fas fa-arrow-left"></i>&nbsp;<b>Back to course</b></a>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row mt-lg-n20 mt-md-n11 mt-n10 col-lg-12 justify-content-center">
+            <div class="col-xl-12 col-lg-12 col-md-12 ">
+              <div class="card z-index-0 mb-4">
+                <div class="card-header text-center pt-4">
+                  <h5>Task <?= $judul?></h5>
+                </div>
+                <div class="card-body mb-5">
+                  <div class="accordion-1">
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-md-10 mx-auto">
+                          <div class="accordion" id="accordionRental">
+                            <div class="accordion-item mb-3">
+                              <h5 class="accordion-header" id="headingOne">
+                                <button class="accordion-button border-bottom font-weight-bold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                  Tugas 8
+                                  <span class="btn bg-gradient-success btn-sm btn-round collapse-close pt-1 position-absolute end-0 me-5"><i class="fa fa-check"></i></span>
+                                  <i class="collapse-close fa fa-chevron-down text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
+                                  <i class="collapse-open fa fa-chevron-down text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
+                                </button>
+                              </h5>
+                              <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionRental" style="">
+                                <div class="accordion-body text-sm opacity-8">
+                                  <div class="card">
+                                    <div class="card-body p-3">
+                                      <div class="row">
+                                        <div class="col-lg-6 mb-lg-0 mb-4">
+                                          <div class="d-flex flex-column h-100">
+                                            <h5 class="font-weight-bolder">Landing Page 1</h5>
+                                            <p class="mb-5">Boostrap 5.</p>
+                                            <a class="text-dark font-weight-bold ps-1 mb-0 icon-move-left mt-auto" href="javascript:;">
+                                              <button class="btn bg-gradient-success btn-sm">
+                                                View 
+                                                <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
+                                              </button>
+                                            </a>
+                                          </div>
+                                        </div>
+                                        <div class="col-lg-6 me-auto ms-0 text-end">
+                                          <div class="bg-gradient-primary border-radius-lg min-height-100">
+                                            <img src="./assets/img/shapes/waves-white.svg" class="position-absolute h-100 top-0 d-md-block d-none" alt="waves">
+                                            <div class="position-relative">
+                                              <img class="max-width-500 w-100 position-relative z-index-2 rounded" src="./assets/img/landing1.png">
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="accordion-item mb-3">
+                              <h5 class="accordion-header" id="headingTwo">
+                                <button class="accordion-button border-bottom font-weight-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                  Tugas 9
+                                  <span class="btn bg-gradient-success btn-sm btn-round collapse-close pt-1 position-absolute end-0 me-5"><i class="fa fa-check"></i></span>
+                                  <i class="collapse-close fa fa-chevron-down text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
+                                  <i class="collapse-open fa fa-chevron-down text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
+                                </button>
+                              </h5>
+                              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionRental">
+                                <div class="accordion-body text-sm opacity-8">
+                                    <div class="card">
+                                      <div class="card-body p-3">
+                                        <div class="row">
+                                          <div class="col-lg-6 mb-lg-0 mb-4">
+                                            <div class="d-flex flex-column h-100">
+                                              <h5 class="font-weight-bolder">Landing Page 2</h5>
+                                              <p class="mb-5">Boostrap 5.</p>
+                                              <a class="text-dark font-weight-bold ps-1 mb-0 icon-move-left mt-auto" href="https://sib-peserta1.nurulfikri.com/tugas9/">
+                                                <button class="btn bg-gradient-success btn-sm">
+                                                  View
+                                                  <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
+                                                </button>
+                                              </a>
+                                            </div>
+                                          </div>
+                                          <div class="col-lg-6 me-auto ms-0 text-end">
+                                            <div class="bg-gradient-primary border-radius-lg min-height-100">
+                                              <img src="./assets/img/shapes/waves-white.svg" class="position-absolute h-100 top-0 d-md-block d-none" alt="waves">
+                                              <div class="position-relative">
+                                                <img class="max-width-500 w-100 position-relative z-index-2 rounded" src="./assets/img/landing2.png">
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="accordion-item mb-3">
+                              <h5 class="accordion-header" id="headingThree">
+                                <button class="accordion-button border-bottom font-weight-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                  Tugas 10
+                                  <span class="btn bg-gradient-success btn-sm btn-round collapse-close pt-1 position-absolute end-0 me-5"><i class="fa fa-check"></i></span>
+                                  <i class="collapse-close fa fa-chevron-down text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
+                                  <i class="collapse-open fa fa-chevron-down text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
+                                </button>
+                              </h5>
+                              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionRental">
+                                <div class="accordion-body text-sm opacity-8">
+                                  <div class="card">
+                                      <div class="card-body p-3">
+                                        <div class="row">
+                                          <div class="col-lg-6 mb-lg-0 mb-4">
+                                            <div class="d-flex flex-column h-100">
+                                              <h5 class="font-weight-bolder">Admin Page </h5>
+                                              <p class="mb-5">Boostrap 5.</p>
+                                              <a class="text-dark font-weight-bold ps-1 mb-0 icon-move-left mt-auto" href="javascript:;">
+                                                <button class="btn bg-gradient-success btn-sm">
+                                                  View
+                                                  <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
+                                                </button>
+                                              </a>
+                                            </div>
+                                          </div>
+                                          <div class="col-lg-6 me-auto ms-0 text-end">
+                                            <div class="bg-gradient-primary border-radius-lg min-height-100">
+                                              <img src="./assets/img/shapes/waves-white.svg" class="position-absolute h-100 top-0 d-md-block d-none" alt="waves">
+                                              <div class="position-relative">
+                                                <img class="max-width-500 w-100 position-relative z-index-2 rounded" src="./assets/img/admin.png">
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item mb-3">
+                              <h5 class="accordion-header" id="headingFour">
+                                <button class="accordion-button border-bottom font-weight-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                  Tugas 11
+                                  <span class="btn bg-gradient-success btn-sm btn-round collapse-close pt-1 position-absolute end-0 me-5"><i class="fa fa-check"></i></span>
+                                  <i class="collapse-close fa fa-chevron-down text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
+                                  <i class="collapse-open fa fa-chevron-down text-xs pt-1 position-absolute end-0 me-3" aria-hidden="true"></i>
+                                </button>
+                              </h5>
+                              <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionRental">
+                                <div class="accordion-body text-sm opacity-8">
+                                <div class="card">
+                                      <div class="card-body p-3">
+                                        <div class="row">
+                                          <div class="col-lg-6 mb-lg-0 mb-4">
+                                            <div class="d-flex flex-column h-100">
+                                              <h5 class="font-weight-bolder">Landing Page Index</h5>
+                                              <p class="mb-5">Boostrap 5.</p>
+                                              <a class="text-dark font-weight-bold ps-1 mb-0 icon-move-left mt-auto" href="javascript:;">
+                                                <button class="btn bg-gradient-success btn-sm">
+                                                  View
+                                                  <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
+                                                </button>
+                                              </a>
+                                            </div>
+                                          </div>
+                                          <div class="col-lg-6 me-auto ms-0 text-end">
+                                            <div class="bg-gradient-primary border-radius-lg min-height-100">
+                                              <img src="./assets/img/shapes/waves-white.svg" class="position-absolute h-100 top-0 d-md-block d-none" alt="waves">
+                                              <div class="position-relative">
+                                                <img class="max-width-500 w-100 position-relative z-index-2 rounded" src="./assets/img/landing.png">
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </main>
+    <?php include_once 'social_media.php'?>
