0757dae0fef1ab9da38b4eda48898859ef8dc945
abdulaziz1999 <azizmentor96@gmail.com>
2022-04-13 14:11:37 +0700

    page main routing

--- PHP/landing/detail/main.php
+++ PHP/landing/detail/main.php
@@ -2,7 +2,8 @@
     @$req = $_REQUEST['materi'];
     if(empty($req)){
         include_once 'home.php';
-    }elseif ($req == 'home' || $req == 'php' || $req == 'javascript' || $req == 'html&css' || $req == 'mysql') {
+    }elseif ($req == 'home' || $req == 'php' || $req == 'javascript' || $req == 'designweb' || 
+             $req == 'uiux' || $req == 'github' || $req == 'laravel' || $req == 'mysql' || $req == 'restapi') {
         include_once $req . '.php';
     } else {
         include_once '404.php';
